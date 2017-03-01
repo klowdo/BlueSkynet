4cfa0562cdc47acf127e5e17dbbaf29edde75552
klowdo <klowdo.fs@gmail.com>
2017-03-01 19:33:32 +0100

    Added ServiceBusConnectionRm

---
+++ Core/BlueSkynet.Infrastructure/ReadModels/ServiceBus/ServiceBusConnectionRm.cs
@@ -0,0 +1,48 @@
+﻿using BlueSkynet.Domain.Bus;
+using BlueSkynet.Domain.Data;
+using BlueSkynet.Domain.Models;
+using BlueSkynet.Domain.Models.ServiceBus.Events;
+
+namespace BlueSkynet.Infrastructure.ReadModels.ServiceBus
+{
+    public class ServiceBusConnectionRm : Entity
+    {
+        public ServiceBusConnectionRm()
+        {
+        }
+
+        public ServiceBusConnectionRm(string name, string connectionString)
+        {
+            Name = name;
+            ConnectionString = connectionString;
+        }
+
+        public string Name { get; set; }
+        public string ConnectionString { get; set; }
+    }
+
+    public class ServiveBusConnectionView :
+        ReadModelBase<ServiceBusConnectionRm>,
+         IHandles<ServiceBusCreated>,
+        IHandles<ServiceBusRenamed>
+    {
+        public ServiveBusConnectionView(IDataContext db) : base(db)
+        {
+        }
+
+        public void Handle(ServiceBusCreated message)
+        {
+            Insert(new ServiceBusConnectionRm(
+                name: message.Name,
+                connectionString: message.ConnectionString
+                ));
+        }
+
+        public void Handle(ServiceBusRenamed message)
+        {
+            var item = Find(message.Id);
+            item.Name = message.Name;
+            Update(item);
+        }
+    }
+}
